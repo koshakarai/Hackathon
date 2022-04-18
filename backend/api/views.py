3d8ccc191dc6b37c105a5cef9cbe297523d8a3c0
koshakarai <akay.webdevelopment@gmail.com>
2022-04-18 03:29:40 +0300

    Test api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-def index(request):
-    return HttpResponse("asd")
+from .serializer import *
+
+@api_view(['GET', 'POST'])
+def test_api(request):
+    """
+    The test api
+ """
+    if request.method == 'GET':
+        data = [{"id":0, "name":"Akay"},
+        {"id":1, "name":"Ararat"},
+        {"id":2, "name":"Maxim"},
+        {"id":3, "name":"Roman"}] 
+
+        return Response(data)
+
+    elif request.method == 'POST':
+        return Response({"hi":"hi"})
