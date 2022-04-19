54bbff3262bc0af72e86f399f884c8791ac676b3
artj15 <artj15@gmail.com>
2022-04-19 15:17:48 +0300

    Restore files

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,6 +9,7 @@ class Users(models.Model):
     company_name = models.TextField()
     inn = models.BigIntegerField()
     password = models.TextField()
-
+    type = models.CharField(max_length=10)
+    
     def __str__(self):
         return self.name
