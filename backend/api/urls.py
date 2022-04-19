57ebb6999909ce98cb9ebe3bdd99040551808139
artj15 <artj15@gmail.com>
2022-04-19 15:10:44 +0300

    Files added.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.test_api),
-    path('v1/create', views.create_user)
+    path('v1/create', views.create_user),
+    path('v1/auth', views.auth_user),
+
 ]
